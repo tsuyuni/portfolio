198f24c4bc291e69b8bc8068bdd32436d0329899
梅雨 <yuri.tsuchikawa@gmail.com>
2022-12-20 20:27:37 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-$(document.fonts).ready(function(fontFaceSet) {
-  $("#loading").delay(2000).queue(function(){
-  	$(this).css({"opacity":"0"});
-  	$("body").css({"overflow":"scroll"})
-  	$(this).delay(1000).queue(function(){
-  		$(this).css({"display":"none"});
-  	});
-  });
+document.fonts.ready.then(function() {
+	$("#loading").delay(2000).queue(function(){
+	  	$(this).css({"opacity":"0"});
+	  	$("body").css({"overflow":"scroll"})
+	  	$(this).delay(1000).queue(function(){
+	  		$(this).css({"display":"none"});
+		});
+	});
 	console.log("test");
 });
 
