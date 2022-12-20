07f34fc422b0c1394e29805c533a7bf01a1be712
梅雨 <yuri.tsuchikawa@gmail.com>
2022-12-20 14:30:37 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-document.fonts.ready.then(function(fontFaceSet) {
-  $("#loading").css({"display":"none"});
+$(document.fonts).ready(function(fontFaceSet) {
+  $("#loading").delay(2000).queue(function(){
+  	$(this).css({"opacity":"0"});
+  	$(this).delay(1000).queue(function(){
+  		$(this).css({"display":"none"});
+  	});
+  });
 	console.log("test");
 });
 
