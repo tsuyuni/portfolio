d8a8fecf4406e17695683f37923b35c21c38b6a0
梅雨 <yuri.tsuchikawa@gmail.com>
2022-12-20 14:41:37 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document.fonts).ready(function(fontFaceSet) {
   $("#loading").delay(2000).queue(function(){
   	$(this).css({"opacity":"0"});
+  	$("body").css({"overflow":"scroll"})
   	$(this).delay(1000).queue(function(){
   		$(this).css({"display":"none"});
   	});
