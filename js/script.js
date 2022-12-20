a03927d1c7fc6aab7c03aaccca3c163f501e7acc
梅雨 <yuri.tsuchikawa@gmail.com>
2022-12-20 14:07:13 +0900

    ローディング画面を追加

--- js/script.js
+++ js/script.js
@@ -1,41 +1,5 @@
-// script.js
-
-// const typewriter = (param) => {
-
-// 	let el = document.querySelector(param.el);
-// 	console.log(el);
-// 	let speed = param.speed;
-// 	let string = param.string.split("");
-
-// 	string.forEach((char,index) => {
-// 		setTimeout(() => {
-// 			el.textContent += char;
-// 		}, speed * index);
-// 	});
-
-// };
-
-
-// window.onload = function(){
-
-// 	typewriter({
-// 		el: "#typewriter",
-// 		string: "コーディング",
-// 		speed: 300
-// 	});
-
-// }
-
-// let p = document.querySelector("#subtitle");
-// console.log(p);
-
-// window.addEventListener("scroll", function(){
-// 	let y = window.pageYOffset;
-// 	p.style.color = "rgba(204,204,204," + y / window.height + ")";
-// }, false);
-
-
-	
-
-
+$(function() {
+	$("#loading").css({"display":"none"});
+	console.log("test");
+});
 
